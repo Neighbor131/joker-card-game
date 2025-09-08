b487bffee855701e2f23700218d357d3a45ee5d7
Neighbor131 <127187938+Neighbor131@users.noreply.github.com>
2025-09-08 11:22:12 +0400

    Update CardView.tsx

--- src/components/CardView.tsx
+++ src/components/CardView.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Card } from '../game/types'
 import { cardAssetFile } from '../game/deck'
 
-export default function CardView({card, hidden=false, disabled=false, onClick}:{card:Card, hidden?:boolean, disabled?:boolean, onClick?:()=>void}){
-  const src = hidden ? '/cards/2B.svg' : `/cards/${cardAssetFile(card)}`
+const BASE = import.meta.env.BASE_URL ?? '/'
+
+export default function CardView({
+  card, hidden=false, disabled=false, onClick
+}:{card:Card, hidden?:boolean, disabled?:boolean, onClick?:()=>void}){
+  const src = hidden
+    ? `${BASE}cards/2B.svg`
+    : `${BASE}cards/${cardAssetFile(card)}`
   return (
-    <div className={"card " + (disabled?'disabled':'')} onClick={disabled?undefined:onClick}>
+    <div className={'card ' + (disabled?'disabled':'')} onClick={disabled?undefined:onClick}>
       <img src={src} alt={hidden?'Back':`${card.rank}${card.suit}`} />
-      {card.isJoker && !hidden && (
-        <div className="jokerRibbon">JOKER</div>
-      )}
+      {card.isJoker && !hidden && <div className="jokerRibbon">JOKER</div>}
     </div>
   )
 }
