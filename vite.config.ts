529fab6862d4b142dd0f4775b1f717b67f32222f
Neighbor131 <127187938+Neighbor131@users.noreply.github.com>
2025-09-08 11:12:17 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // must match your repo name exactly
-  base: '/joker-card-game/',
-  server: { port: 5173, open: true }, // harmless locally
+  base: '/joker-card-game/',   // must match the repo name
 })
