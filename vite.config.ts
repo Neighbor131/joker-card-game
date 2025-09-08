94eecc93dfd313908698237c4a0e520e7bfd2787
Neighbor131 <127187938+Neighbor131@users.noreply.github.com>
2025-09-08 12:22:57 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 export default defineConfig({
   base: '/joker-card-game/',
-  build: { sourcemap: true }
+  build: { sourcemap: true } // so errors point to TSX lines
 })
