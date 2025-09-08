75961b5704db4a993ec984dd3f53c5a0957d7050
Neighbor131 <127187938+Neighbor131@users.noreply.github.com>
2025-09-08 12:23:21 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+
+console.log('JOKER BUILD:', 'v8')  // ← change this number on each deploy
+
 createRoot(document.getElementById('root')!).render(<App />)
